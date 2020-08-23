ff30eea05001d2d0823456637a0cfffd048f14e8
phelipe <phelipe_teles@hotmail.com>
2020-08-23 19:33:52 -0300

    Remove tests docstring

--- tests/test_index.py
+++ tests/test_index.py
@@ -9,44 +9,37 @@ URL = "https://google.com"
 
 
 def test_home(client):
-    """GET / should return 200 OK."""
     response = client.get("/")
     assert response.status_code == 200
 
 
 def test_new(client, db):
-    """POST /new should insert shorten and insert URL into database."""
     response = client.post("/new", json={"url": URL})
     assert response.status_code == 200
 
 
 def test_new_with_alias(client, db):
-    """POST /new with custom alias."""
     response = client.post("/new", json={"alias": "myalias", "url": URL})
     assert response.json["short_url"] == "myalias"
     assert response.status_code == 200
 
 
 def test_new_already_shortened_url(client, db):
-    """POST /new already shortened URL should not fail."""
     response = client.post("/new", json={"url": URL})
     assert response.status_code == 200
 
 
 def test_new_invalid_uri_schemes(client, db):
-    """POST /new with invalid URI scheme should return 400 BAD REQUEST."""
     response = client.post("/new", json={"url": "javascript:void(0)"})
     assert response.status_code == 400
 
 
 def test_new_no_network_location(client, db):
-    """POST /new with no network location should return 400 BAD REQUEST."""
     response = client.post("/new", json={"url": "https://"})
     assert response.status_code == 400
 
 
 def test_redirect(client, db):
-    """GET /<short_url> should return 302 FOUND."""
     short_url = Url.query.filter_by(url=URL).first().short_url
 
     response = client.get(f"/{short_url}")
@@ -55,6 +48,5 @@ def test_redirect(client, db):
 
 
 def test_failed_redirect(client):
-    """GET /<short_url> should return 404 NOT FOUND."""
     response = client.get("/45123")
     assert response.status_code == 404
