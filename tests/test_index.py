a231bc22121f735fa179654cd92f05629994fe20
phelipe <phelipe_teles@hotmail.com>
2020-08-23 19:36:00 -0300

    Add test for redirecting with alias. Rename some tests

--- tests/test_index.py
+++ tests/test_index.py
@@ -8,7 +8,7 @@ from app.db import Url
 URL = "https://google.com"
 
 
-def test_home(client):
+def test_index(client):
     response = client.get("/")
     assert response.status_code == 200
 
@@ -16,27 +16,6 @@ def test_home(client):
 def test_new(client, db):
     response = client.post("/new", json={"url": URL})
     assert response.status_code == 200
-
-
-def test_new_with_alias(client, db):
-    response = client.post("/new", json={"alias": "myalias", "url": URL})
-    assert response.json["short_url"] == "myalias"
-    assert response.status_code == 200
-
-
-def test_new_already_shortened_url(client, db):
-    response = client.post("/new", json={"url": URL})
-    assert response.status_code == 200
-
-
-def test_new_invalid_uri_schemes(client, db):
-    response = client.post("/new", json={"url": "javascript:void(0)"})
-    assert response.status_code == 400
-
-
-def test_new_no_network_location(client, db):
-    response = client.post("/new", json={"url": "https://"})
-    assert response.status_code == 400
 
 
 def test_redirect(client, db):
@@ -47,6 +26,33 @@ def test_redirect(client, db):
     assert response.headers["Location"] == html.escape(URL)
 
 
-def test_failed_redirect(client):
+def test_new_with_alias(client, db):
+    response = client.post("/new", json={"alias": "my-site", "url": URL})
+    assert response.json["short_url"] == "my-site"
+    assert response.status_code == 200
+
+
+def test_redirect_with_alias(client, db):
+    response = client.get("/my-site")
+    assert response.status_code == 302
+    assert response.headers["Location"] == html.escape(URL)
+
+
+def test_already_shortened_url(client, db):
+    response = client.post("/new", json={"url": URL})
+    assert response.status_code == 200
+
+
+def test_invalid_uri_schemes(client, db):
+    response = client.post("/new", json={"url": "javascript:void(0)"})
+    assert response.status_code == 400
+
+
+def test_no_network_location(client, db):
+    response = client.post("/new", json={"url": "https://"})
+    assert response.status_code == 400
+
+
+def test_missing_short_url(client):
     response = client.get("/45123")
     assert response.status_code == 404
