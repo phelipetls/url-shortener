30841691078f78fd55e60a05f47b49229957cccb
phelipe <phelipe_teles@hotmail.com>
2020-08-24 17:17:02 -0300

    Apply black

--- app/db.py
+++ app/db.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class Url(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     url = db.Column(db.String, nullable=False, unique=False)
