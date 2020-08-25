8da33457093681a97718cb6687f4826f8cc5cd84
phelipe <phelipe_teles@hotmail.com>
2020-08-25 08:19:43 -0300

    Update front end files

--- app/static/app.js
+++ app/static/app.js
@@ -1,16 +1,32 @@
 const app = new Vue({
   el: "#app",
   data: {
-    output: null
+    error: null,
+    short_url: null
+  },
+  computed: {
+    shortUrlLink: function() {
+      return `/${this.short_url}`
+    },
   },
   methods: {
-    async getShortUrl(url, expiration_date = null) {
+    async getShortUrl(url, alias) {
+      url = document.getElementById("url").value;
+      alias = document.getElementById("alias").value;
+
       const response = await fetch("/new", {
+        method: "POST",
         headers: { "Content-type": "application/json" },
-        body: JSON.stringify({ url, expiration_date })
+        body: JSON.stringify({ url, alias })
       });
-      const json = await response.json();
-      this.output = json["error"] || json["short_url"]
+
+      const { error, short_url } = await response.json();
+      this.error = error;
+      this.short_url = short_url;
+    },
+    copyShortUrl() {
+      const shortUrl = document.querySelector(".short-url").href;
+      navigator.clipboard.writeText(shortUrl);
     }
   }
 });
