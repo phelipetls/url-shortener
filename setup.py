b0daf982db54c90e243f5a8fbace4ba51a2f354c
phelipe <phelipe_teles@hotmail.com>
2020-08-26 18:36:05 -0300

    Revert "Require python >= 3.7"
    This reverts commit dc1b869aaed7bcb502418b1de07498ccec734b16.

--- setup.py
+++ setup.py
@@ -5,6 +5,5 @@ setup(
     description="A url shortener made for learning",
     packages=find_packages(),
     include_package_data=True,
-    python_requires=">=3.7",
     install_requires=["flask", "flask-sqlalchemy", "psycopg2", "python-dotenv", "python-dateutil", "gunicorn"],
 )
