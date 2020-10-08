2368636773fed2b558e0625c5462f1e8f112b2ea
phelipe <phelipe_teles@hotmail.com>
2020-10-07 22:42:33 -0300

    Refactor config tests slightly

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -5,10 +5,10 @@ from app import create_app
 def test_config_development():
     app = create_app()
     assert not app.testing
-    assert "postgres" in app.config["SQLALCHEMY_DATABASE_URI"]
+    assert app.config["SQLALCHEMY_DATABASE_URI"].startswith("postgresql+psycopg2")
 
 
 def test_config_testing():
     app = create_app("config.TestConfig")
     assert app.testing
-    assert re.match("sqlite:////tmp/", app.config["SQLALCHEMY_DATABASE_URI"])
+    assert app.config["SQLALCHEMY_DATABASE_URI"].startswith("sqlite:////tmp/")
